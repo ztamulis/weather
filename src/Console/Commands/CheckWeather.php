e94463caf133190b47b3ea0f5bb31bb45eb81f0d
Zygintas Tamulis <zygintas.tamulis@gmail.com>
2018-08-02 15:22:54 +0300

    add task scheduler and wind speed check

---
+++ src/Console/Commands/CheckWeather.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bene\Weather\Console\Commands;
+
+use Bene\Weather\City;
+use Bene\Weather\UserEmail;
+use Bene\Weather\Weather;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class CheckWeather extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:weather';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle()
+    {
+        $city = City::orderBy('created_at', 'DESC')->first();
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' . $city->city . '&APPID=6f3aa95336edac6e5b6296a6e5f37bf2');
+        $res = json_decode($res->getBody()->getContents(), true);
+        $lastWind = Weather::orderBy('created_at', 'DESC')->first();
+        if ($lastWind['is_more_than_10'] == '0' && $res['wind']['speed'] > 10) {
+            DB::table('weathers')->insert(['is_more_than_10' => '1', 'created_at' => date('Y/m/d H:i:s')]);
+        } else if ($lastWind['is_more_than_10'] == '1' && $res['wind']['speed'] < 10) {
+            DB::table('weathers')->insert(['is_more_than_10' => '0', 'created_at' => date('Y/m/d H:i:s')]);
+        }
+
+    }
+
+
+
+}
---
+++ src/Console/Kernel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bene\Weather\Console;
+
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'Bene\Weather\Console\Commands\CheckWeather'
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+//        $schedule->command('inspire')
+//            ->hourly();
+        $schedule->command('check:weather')->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+    }
+}
